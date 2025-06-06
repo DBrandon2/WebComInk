d6709a7c6d6cf3785f1e809f13b551c98bd97e4b
DBrandon2 <137073865+DBrandon2@users.noreply.github.com>
2025-06-06 16:22:35 +0200

    Update LatestRelease.jsx

--- WebComInk Client/src/pages/Homepage/LatestRelease.jsx
+++ WebComInk Client/src/pages/Homepage/LatestRelease.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import image1 from "../../assets/MangaCover/kaiju cover.webp";
 import image2 from "../../assets/MangaCover/OP manga cover.jpg";
-import image3 from "../../assets/MangaCover/vinland-Saga-28.webp";
+import image3 from "../../assets/MangaCover/Vinland-Saga-28.webp";
 import image4 from "../../assets/MangaCover/Sakamoto Cover.webp";
 import ButtonAnimated from "../../components/ButtonAnimated";
 import { IoIosArrowDown } from "react-icons/io";
