6468508cd4e18da4470351f467c14f5f69efff0e
DBrandon2 <137073865+DBrandon2@users.noreply.github.com>
2025-06-13 12:56:22 +0200

    test proxy prod

--- WebComInk Server/api/proxy.js
+++ WebComInk Server/api/proxy.js
@@ -7,11 +7,13 @@ router.get("/covers/:mangaId/:fileName", async (req, res) => {
     const { mangaId, fileName } = req.params;
 
     // Exemple : fileName = "7f27c05b-16be-4f97-b317-df6544f6d4b8.256.jpg"
-    // On enlève le ".256" avant l'extension
+    // On enlève le ".256" avant l'extension pour reconstruire l'original
     const originalFileName = fileName.replace(".256", "");
 
-    // Construire l'URL MangaDex avec resize=256
+    // Reconstruit l'URL MangaDex
     const url = `https://uploads.mangadex.org/covers/${mangaId}/${originalFileName}.256.jpg`;
+
+    console.log(`Proxy: ${url}`);
 
     const response = await axios.get(url, {
       responseType: "stream",
@@ -21,13 +23,10 @@ router.get("/covers/:mangaId/:fileName", async (req, res) => {
       },
     });
 
-    // Propager le Content-Type reçu
     res.setHeader("Content-Type", response.headers["content-type"]);
-
-    // Streamer la réponse vers le client
     response.data.pipe(res);
   } catch (error) {
-    console.error("Erreur proxy :", error.message);
+    console.error("Erreur proxy :", error.message, error?.response?.status);
     res.status(500).send("Erreur lors du proxy");
   }
 });
