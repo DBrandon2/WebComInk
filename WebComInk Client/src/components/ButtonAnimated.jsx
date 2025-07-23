1f0e74175130b9722c18088ee2eb0a3a7f05eee8
DBrandon2 <137073865+DBrandon2@users.noreply.github.com>
2025-07-23 15:15:47 +0200

    update

--- WebComInk Client/src/components/ButtonAnimated.jsx
+++ WebComInk Client/src/components/ButtonAnimated.jsx
@@ -7,7 +7,7 @@ export default function ButtonAnimated({ text, justify = "justify-end", onClick 
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
         className="px-10 flex items-center gap-x-2 py-2 bg-white text-dark-bg rounded-md shadow transition-colors sm:px-16 sm:py-3 cursor-pointer font-medium tracking-widest"
-        onClick={onClick} // Ajoutez cette ligne
+        onClick={onClick}
       >
         {text}
       </motion.button>
